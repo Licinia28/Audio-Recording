fc885f44878cf7233ea14489ca21ef4d063eab31
unknown <mliciniatema98@gmail.com>
2022-09-27 09:39:29 +0200

    updated

--- App.js
+++ App.js
@@ -1,10 +1,87 @@
+import Recact from 'react';
+import {Audio} from 'expo-av';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+
+export default function App()
+  {
+    const [recording, setRecording] = React.useState();
+    const [recordings, setRecordings] = React.useState([]);
+    const [message, setMessage] = React.useState("");
+
+    async function startRecording()
+      {
+        try{
+          const permission = await Audio.requestPermissionsAsync();
+          if(permission.status === "granted")
+          {
+            await Audio.setAudioModeAsync({
+              allowRecordingIOS:true,
+              playsInSilentModeIOS:true
+            });
+            const {recording} = await Audio.Recording.createAsync(
+              Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+            );
+            setRecording(recording);
+          }
+          else{
+            setMessage("Please grant permission to access microphone");
+          }
+        }
+          catch(err)
+          {
+            console.error ('Failed to strat recording', err);
+          }
+        }
+     
+async function stopRecording()
+  {
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+
+    let updateRecordings = [...recording];
+
+    const {sound, status} = await recording.createNewLoadedSoundAsync();
+    updateRecordings.push({
+      sound:sound,
+      duration:getDurationFormatted(status.durationMillis),
+      file:recording.getURI()
+    });
+    setRecordings(updateRecordings);
+  }
+
+function getDurationFormatted(millis)
+{
+  const minutes = millis / 1000 / 60;
+  const minutesDisplay = Math.floor (minutes);
+  const seconds = Math.round((minutes - minutesDisplay) * 60);
+  const secondsDisplay = seconds < 10 ? '0${seconds}': seconds;
+  return '${minutesDisplay} : ${secondsDisplay}';
+}
+
+function getRecordingLines()
+  {
+    return recordings.map((recordingLine, index) => {
+      return(
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>
+            Recording{index + 1} - {recordingLine.duration}
+          </Text>
+          <Button style = {styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+        </View>
+      );
+    });
+  }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Message</Text>
+      <Button title={recording ? 'Stop Recording': 'Start Recording'}
+        onPress={recording ? stopRecording:startRecording} />
+        {getRecordingLines()}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +94,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row:{
+    flexDirection : row,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fill:{
+    flex: 1,
+    margin: 16,
+  },
+   button:{
+    margin:16
+  }
 });
